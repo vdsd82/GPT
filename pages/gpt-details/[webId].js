552aca8ec19aa1f458eab8821f4e835aafefc2ee
vdsd82 <vijaydhanda@gmail.com>
2023-11-19 04:43:11 +0530

    commit

---
+++ pages/gpt-details/[webId].js
@@ -0,0 +1,69 @@
+// pages/gpt-details/[webId].js
+
+import React from "react";
+import GPTDetailsArea from "../../components/GPTDetails/GPTDetailsArea";
+import BgShape from "../../components/common/BgShape";
+import Header from "../../components/Home/Header";
+import SEO from "../../components/seo";
+import Footer from "../../components/Home/Footer";
+import { findDocumentByWebId, findAllDocuments } from "../../utils/mongodbUtil";
+
+const gptDetails = ({ post }) => {
+  // Ensure that post is not null before trying to access its properties
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <>
+      <SEO pageTitle={"GPT Details"} />
+      <Header />
+      <BgShape />
+      <GPTDetailsArea webId={post.web_id} />
+      <Footer />
+    </>
+  );
+};
+
+export default gptDetails;
+
+export async function getStaticProps({ params }) {
+  try {
+    const post = await findDocumentByWebId(params.webId);
+
+    if (!post) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        post,
+      },
+      revalidate: 86400, // In seconds
+    };
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export async function getStaticPaths() {
+  try {
+    const posts = await findAllDocuments();
+    console.log(posts);
+    const paths = posts.map((post) => {
+      // Make sure `web_id` is a property in your documents
+      // Also ensure that `web_id` is converted to a string if it's not already
+      return { params: { webId: post.web_id.toString() } };
+    });
+
+    return { paths, fallback: false };
+  } catch (error) {
+    console.error("Error fetching paths:", error);
+    return { paths: [], fallback: false };
+  }
+}
---
+++ utils/mongodbUtil.js
@@ -0,0 +1,51 @@
+// utils/mongodbUtil.js
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const dbName = "GPT-Combined";
+
+let client;
+let clientPromise;
+
+// Create a single MongoClient instance that will be reused across requests
+// This approach assumes you are using MongoDB 3.6 or later
+if (!client) {
+  client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  clientPromise = client.connect();
+}
+
+async function connectToDatabase() {
+  // Wait for the client connection promise to resolve
+  await clientPromise;
+  return client.db(dbName);
+}
+
+export async function findDocumentByWebId(web_id) {
+  const db = await connectToDatabase();
+  const collection = db.collection("CombinedData");
+  const document = await collection.findOne({ web_id: parseInt(web_id) });
+  return document;
+}
+
+export async function findAllDocuments() {
+  const db = await connectToDatabase();
+  const collection = db.collection("CombinedData");
+  const documents = await collection.find({}).toArray();
+  return documents;
+}
+
+export async function findDocumentsWithTextSearch(searchTerm) {
+  const db = await connectToDatabase();
+  const collection = db.collection("CombinedData");
+  const documents = await collection
+    .find(
+      { $text: { $search: searchTerm } },
+      { projection: { score: { $meta: "textScore" } } }
+    )
+    .sort({ score: { $meta: "textScore" } })
+    .toArray();
+  return documents;
+}
